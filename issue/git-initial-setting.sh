82b5b3c999f10d0132b097a32eadb4e9b3b383bb
Hong Il Young <cavrena@gmail.com>
2023-02-09 16:50:07 +0900

    주석 수정

--- issue/git-initial-setting.sh
+++ issue/git-initial-setting.sh
@@ -3,10 +3,10 @@
 ##############################################################
 # Account Setting
 $ git config --global user.name [USER_NM]
-$ git config --global user.name "Hong Il Young"
 $ git config --global user.email [USER_EMAIL]
-$ git config --global user.email "cavrena@gmail.com"        # git 가입 이메일
 
+$ git config --global user.name "Hong Il Young"
+$ git config --global user.email "cavrena@gmail.com"        # git 가입 이메일
 
 # 설정 확인
 $ git config --list
